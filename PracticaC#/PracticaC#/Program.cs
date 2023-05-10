8b67d7640c32cd6654a266207ddbc539cf49c346
DANIEL TELLEZ <danieleduardotellezana@gmail.com>
2023-05-10 13:45:14 -0500

    Finish date class

---
+++ PracticaC#/PracticaC#/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using PracticaC_;
+
+Console.WriteLine("Hello, World!");
+
+try
+{
+	Console.WriteLine(new Date(2022, 11, 2));
+	Console.WriteLine(new Date(2022, 6, 30));
+	Console.WriteLine(new Date(2022, 11, 2));
+
+}
+catch (Exception error)
+{
+	Console.WriteLine(error.Message);
+	throw;
+}
